0a36c76948e57bef430cc6a129623f1cbde2cea5
MKHDatascientist <117984281+MKHDatascientist@users.noreply.github.com>
2022-11-24 19:36:47 +0100

    Update titanic-feature-pipeline.py

--- titanic-feature-pipeline.py
+++ titanic-feature-pipeline.py
@@ -11,26 +11,15 @@ import hops.featurestore as featureS#???
 project = hopsworks.login()
 fs = project.get_feature_store()
 
-#titanic_df = pd.read_csv('https://raw.githubusercontent.com/ID2223KTH/id2223kth.github.io/master/assignments/lab1/titanic.csv')#
-
-#titanic_df = pd.read_csv("https://raw.githubusercontent.com/ID2223KTH/id2223kth.github.io/master/assignments/lab1/titanic.csv")
-#titanic_df = pd.read_csv("https://repo.hops.works/master/hopsworks-tutorials/data/iris.csv")
 titanic_df = pd.read_csv('Dataset/cleanedTitanic.csv')
 ###################################################################################################
 #To create a feature group you need to give it a name and specify a primary key
-titan_fg = fs.get_or_create_feature_group(
-    name="titan_modal",
-    version=53,
-    primary_key=['PassengerId'],# 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp','Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'],
-    description="TitanicDataset")
+titanic_fg = fs.get_or_create_feature_group(
+    name="titanic_modal",
+    version=55,
+    primary_key=['PassengerId','Pclass','Numeric_sex','Age',"Fare"]#['PassengerId']
+    ,description="TitanicDataset")
 
-titan_fg.insert(titanic_df, write_options={"wait_for_job" : False})
+titanic_fg.insert(titanic_df, write_options={"wait_for_job" : False})
 
 ###################################################################################################
-#expectation_suite = ge.core.ExpectationSuite(expectation_suite_name="iris_dimensions")    
-#value_between(expectation_suite, "sepal_length", 4.5, 8.0)
-#value_between(expectation_suite, "sepal_width", 2.1, 4.5)
-#value_between(expectation_suite, "petal_length", 1.2, 7)
-#value_between(expectation_suite, "petal_width", 0.2, 2.5)
-#iris_fg.save_expectation_suite(expectation_suite=expectation_suite, validation_ingestion_policy="STRICT")    
-    
